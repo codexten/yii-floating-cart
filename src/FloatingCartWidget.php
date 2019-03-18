d2051a61a4e96e42d470abab0c51c64b3b3036dc
Jomon Johnson <jomon.entero@gmail.com>
2019-03-18 16:47:20 +0530

    init

---
+++ src/FloatingCartWidget.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomon
+ * Date: 18/3/19
+ * Time: 4:44 PM
+ */
+
+namespace codexten\yii\floatingCart;
+
+use yii\base\Widget;
+
+class FloatingCartWidget extends Widget
+{
+
+    public function run()
+    {
+        return $this->render('');
+    }
+}
