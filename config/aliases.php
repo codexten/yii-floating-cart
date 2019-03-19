f3364dedb7d9432f250fdd40140b42560fb02231
Jomon Johnson <jomon.entero@gmail.com>
2019-03-19 11:09:24 +0530

    init

---
+++ config/aliases.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomon
+ * Date: 19/3/19
+ * Time: 11:07 AM
+ */
+
+return [
+    'aliases' => [
+        '@codexten/yii/floatingCart' => '@vendor/codexten/yii-floating-cart/src',
+    ],
+];
